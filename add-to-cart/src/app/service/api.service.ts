de43f0fccb2ea6aee7df8f8fc3ace0709004b6ce
maryline321 <marylinechebet2@gmail.com>
2022-02-23 23:59:44 +0300

    Angular D

--- add-to-cart/src/app/service/api.service.ts
+++ add-to-cart/src/app/service/api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  // create amethod to call the api
+  getProduct(){
+    return this.http.get("https://fakestoreapi.com/products")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
 }
